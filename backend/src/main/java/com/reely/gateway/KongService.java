a9eeefd6e363b55676253e0d8769d640fa461329
mquang279 <quangm279@gmail.com>
2025-10-14 22:35:02 +0700

    feat: add KongService to send request to kong gateway

---
+++ backend/src/main/java/com/reely/gateway/KongService.java
@@ -0,0 +1,36 @@
+package com.reely.gateway;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+public class KongService {
+    private final WebClient webClient;
+
+    public KongService(WebClient.Builder builder) {
+        this.webClient = builder
+                .baseUrl("http://localhost:8001")
+                .build();
+    }
+
+    public Map<String, Object> createConsumer(String username) {
+        return webClient.post()
+                .uri("/consumers")
+                .bodyValue(Map.of("username", username))
+                .retrieve()
+                .bodyToMono(Map.class)
+                .block();
+    }
+
+    public Map<String, Object> createJwtCredential(String username) {
+        return webClient.post()
+                .uri("/consumers/{username}/jwt", username)
+                .bodyValue(Map.of("algorithm", "HS256"))
+                .retrieve()
+                .bodyToMono(Map.class)
+                .block();
+    }
+
+}
