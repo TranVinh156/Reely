561621857146ea6a09f5f7436c2da09696bed519
TranVinh156 <tranvanvinhab2005@gmail.com>
2025-10-19 20:37:33 +0700

    Create VideoControls.tsx

---
+++ frontend/src/components/Video/VideoControls.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function VideoControls() {
+  return (
+    <div className="absolute left-0 right-0 bottom-0 p-2 pointer-events-auto">
+      {/* Placeholder: we'll implement ProgressBar, Volume, etc. in Phase2 */}
+    </div>
+  );
+}
