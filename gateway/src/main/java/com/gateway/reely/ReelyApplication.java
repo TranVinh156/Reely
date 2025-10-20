e147084efa176d673f14ba884a9e6d4c03efccb0
mquang279 <quangm279@gmail.com>
2025-10-20 21:15:39 +0700

    feat: add jwt authentication to gateway

--- gateway/src/main/java/com/gateway/reely/ReelyApplication.java
+++ gateway/src/main/java/com/gateway/reely/ReelyApplication.java
@@ -2,14 +2,29 @@ package com.gateway.reely;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
 @EnableWebFlux
 public class ReelyApplication {
+	private final String backendUrl = "http://localhost:8080";
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReelyApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("auth-service", p -> p
+						.path("/auth/**")
+						.uri(backendUrl))
+				.route("api-service", p -> p
+						.path("/hello")
+						.uri(backendUrl))
+				.build();
+	}
 }
