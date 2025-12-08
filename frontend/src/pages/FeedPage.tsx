e0021d6b4e2025b5abb4aac0e3f4c3c5455070a5
Mquang279 <quangm279@gmail.com>
2025-12-08 14:23:32 +0700

    chore: add spinning icon when loading

--- frontend/src/pages/FeedPage.tsx
+++ frontend/src/pages/FeedPage.tsx
@@ -2,17 +2,17 @@ import FeedList from "../features/feed/FeedList";
 import Sidebar from "../components/Layout/Sidebar";
 import "@/styles/feedSnap.css";
 
-export default function IndexPage() {
+export default function FeedPage() {
   return (
-    <div className="min-h-screen bg-neutral-900 text-white">
+    <div className="min-h-screen bg-primary text-white">
       <div className="flex gap-6">
         <Sidebar />
-        <main className="flex-1">
+        <div className="flex-1">
           {/* Scroll container for feed with CSS scroll-snap */}
           <div className="feed-snap" data-feed-scroller>
             <FeedList />
           </div>
-        </main>
+        </div>
       </div>
     </div>
   );
