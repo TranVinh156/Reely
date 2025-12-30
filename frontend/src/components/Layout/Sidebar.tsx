eea708b471078c24c2695a0d3822fa4f8330529c
Mquang279 <quangm279@gmail.com>
2025-12-30 22:40:37 +0700

    chore: modifying upload page layout

--- frontend/src/components/Layout/Sidebar.tsx
+++ frontend/src/components/Layout/Sidebar.tsx
@@ -180,8 +180,8 @@ export default function Sidebar() {
 
       <div
         className={`fixed left-20 top-0 h-screen w-[350px] bg-primary border-r border-white/10 z-50 shadow-2xl overflow-hidden transition-transform duration-300 ease-in-out ${isNotificationOpen ? 'translate-x-0' : '-translate-x-full'}`}
-        style={{ transform: isNotificationOpen ? 'translateX(0)' : 'translateX(-100%)'}}
-        >
+        style={{ transform: isNotificationOpen ? 'translateX(0)' : 'translateX(-100%)' }}
+      >
         <Notification onClose={() => setIsNotificationOpen(false)} />
       </div>
     </>
