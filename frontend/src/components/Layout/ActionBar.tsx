199e7ac179ea217e900f84d471d503ddfa7cde2e
Mquang279 <quangm279@gmail.com>
2025-12-31 00:01:33 +0700

    feat: add responsive for action bar

--- frontend/src/components/Layout/ActionBar.tsx
+++ frontend/src/components/Layout/ActionBar.tsx
@@ -46,15 +46,15 @@ const ActionBar = () => {
     }
 
     return (
-        <div className="fixed top-4 right-8 z-50" ref={modalRef}>
+        <div className="fixed top-4 right-4 md:right-8 z-50" ref={modalRef}>
             <div className="bg-gray-700 rounded-4xl flex items-center p-1.5 gap-3">
                 {location.pathname !== '/upload' && (
-                    <NavLink to="/upload" className="flex gap-1.5 text-sm items-center font-semibold hover:bg-gray-500 p-2 rounded-4xl cursor-pointer">
+                    <NavLink to="/upload" className="hidden lg:flex gap-1.5 text-sm items-center font-semibold hover:bg-gray-500 p-2 rounded-4xl cursor-pointer">
                         <Upload size={16} />
                         Upload
                     </NavLink>
                 )}
-                <button className="p-1 rounded-3xl w-10 h-10 bg-gray-500 cursor-pointer" onClick={() => setIsModalOpen(!isModalOpen)}>
+                <button className="lg:p-1 rounded-3xl w-10 h-10 bg-gray-500 cursor-pointer" onClick={() => setIsModalOpen(!isModalOpen)}>
                     {!user?.avatarUrl ?
                         <div className="bg-white w-full h-full rounded-full flex items-center justify-center">
                             <UserIcon className="text-black" size={18} />
@@ -66,6 +66,10 @@ const ActionBar = () => {
             </div>
             {isModalOpen &&
                 <div className="bg-gray-600 rounded-xl border border-gray-400 text-sm font-semibold cursor-pointer p-1 text-white">
+                    <NavLink to="/upload" className="lg:hidden flex py-3 items-center text-center gap-3 hover:bg-gray-700 rounded-xl px-3">
+                        <Upload size={20} />
+                        Upload
+                    </NavLink>
                     <NavLink to={`/users/${user?.username}`} className="flex py-3 items-center text-center gap-3 hover:bg-gray-700 rounded-xl px-3">
                         <UserIcon size={20} />
                         View Profile
