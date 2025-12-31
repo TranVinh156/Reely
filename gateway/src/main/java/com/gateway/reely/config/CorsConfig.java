be4b55d526c33dd010d69167f541637017efa8d1
Mquang279 <quangm279@gmail.com>
2025-12-31 19:48:41 +0700

    fix: modifying cors config of gateway

--- gateway/src/main/java/com/gateway/reely/config/CorsConfig.java
+++ gateway/src/main/java/com/gateway/reely/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        config.setAllowedOrigins(Arrays.asList("https://reely-ruby.vercel.app"));
         config.setAllowedHeaders(Arrays.asList("*"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH",
                 "DELETE", "OPTIONS", "HEAD"));
