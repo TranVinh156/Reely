910339e90e3851191523b44fc970a49dc889cca2
Mquang279 <quangm279@gmail.com>
2026-01-02 23:45:34 +0700

    fix: modifying link in navlink

--- frontend/src/components/Video/ActionButtons.tsx
+++ frontend/src/components/Video/ActionButtons.tsx
@@ -155,7 +155,7 @@ export function ActionButtons({ video }: Props) {
       >
         {/* Avatar */}
         <div className="relative">
-          <NavLink className="block h-12 w-12 overflow-hidden rounded-full border-2 border-white/20" to={`/users/${video.user.id}`}>
+          <NavLink className="block h-12 w-12 overflow-hidden rounded-full border-2 border-white/20" to={`/users/${video.user.username}`}>
             {video.user.avatar ? (
               <img
                 src={video.user.avatar}
